c215711464bff7e7a01eb41f29f4f6cefe987b4c
Nitt <emilmeiton@gmail.com>
2025-08-10 11:37:19 +0200

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -2,6 +2,13 @@ function createPuzzle(width, height) {
   const map = {};
   map.width = width;
   map.height = height;
+
+  map.cells = new Array(width * height).fill(0); // 0 = empty
+
+  for (let i = 0; i < map.cells.length; i++) {
+    map.cells[i] = Math.floor(Math.random() * 3); // 0, 1 and 2 randomly
+  }
+  
   return map;
 }
 
